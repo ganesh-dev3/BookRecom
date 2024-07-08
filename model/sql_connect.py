c86b30e2e789ff1671b0266598075c8892706278
Ganeshpandi <gganesan@cas.org>
2024-07-08 18:18:55 +0530

    pssql connection

--- model/sql_connect.py
+++ model/sql_connect.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy.orm import declarative_base
 from contextlib import asynccontextmanager
 
-DATABASE_URL = 'sqlite+aiosqlite:///booksnew.db'
+#DATABASE_URL = 'sqlite+aiosqlite:///booksnew.db'
+#DATABASE_URL = 'postgresql+asyncpg://<db_username>:<db_secret>@<db_host>:<db_port>/<db_name>'
+DATABASE_URL = 'postgresql+asyncpg://%s:%s@%s:%s/%s'%(db_uname, db_passwd, db_host, db_port, db_name)
 
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 
